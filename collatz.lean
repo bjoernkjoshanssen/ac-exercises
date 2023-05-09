95ec0ac405aa9b6f514545cbe7d734ac357450e3
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2023-05-08 14:17:19 -1000

    Update collatz.lean

--- collatz.lean
+++ collatz.lean
@@ -1,22 +1,22 @@
 import computability.DFA --computability.NA
 open_locale classical
 
-def collatz (n : ℕ) : ℕ :=
+def collatz_function (n : ℕ) : ℕ :=
   if n % 2 = 0 then n / 2 else 3 * n + 1
 
-def collatz_pow (init:ℕ): ℕ →  ℕ :=
+def hailstone (init:ℕ): ℕ →  ℕ :=
 λ pow, nat.rec_on pow init (
-  λ thepow theval, collatz theval
+  λ thepow theval, collatz_function theval
 )
 
-def collatz_iter (bound:ℕ) (init:ℕ) : ℤ := -- https://oeis.org/A006577
-dite (∃ k, k<bound ∧ collatz_pow init k = 1) (
+def steps_required (bound:ℕ) (init:ℕ) : ℤ := -- https://oeis.org/A006577
+dite (∃ k, k<bound ∧ hailstone init k = 1) (
   λ h, ((nat.find h ):ℤ)
 ) (
   λ h, -1
 )
 
-def collatz_iter_sequence (bound len:ℕ) : list ℤ :=
-nat.rec_on len list.nil (λ n seq_ind, seq_ind ++ [collatz_iter bound n])
+def steps_required_sequence (bound len:ℕ) : list ℤ :=
+nat.rec_on len list.nil (λ n seq_ind, seq_ind ++ [steps_required bound n])
 
-#eval collatz_iter_sequence 1000 14
+#eval steps_required_sequence 1000 14
