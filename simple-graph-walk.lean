bc6d260599e219c7e90b0b77e1f4435a9fbcbe48
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2023-05-19 12:23:02 -1000

    Update simple-graph-walk.lean

--- simple-graph-walk.lean
+++ simple-graph-walk.lean
@@ -1,34 +1,5 @@
 import combinatorics.simple_graph.connectivity
 
-theorem second_vertex_in_walk {V : Type} (G : simple_graph V) (v0 v1 : V)
-(hv : v0 ≠ v1)
-(wa : G.walk v0 v1) : ∃ a : V, a ≠ v0 ∧ G.adj v0 a :=
-  exists.elim (
-    simple_graph.walk.exists_eq_cons_of_ne hv wa
-  ) (
-    λ a ha, exists.elim ha (
-      λ hh hhh, exists.elim hhh (
-        λ p' hp', 
-        exists.intro a (
-          and.intro (
-            λ hav₀, G.loopless a (by {rw hav₀.symm at hh,exact hh,})) hh
-        )
-      )
-    )
-  )
-
-def rewrite  {V : Type} {G : simple_graph V} {v₀ a w : V} {k:ℕ}
-  (p' : {pp : G.walk a  w // pp.length=k}) (heq: a = v₀)
-      : {pp : G.walk v₀ w // pp.length=k}
-:=
-by {rw heq at p',exact p',}
-
-/-
-Given v₀ and v₁, let w be neither of them.
-For each walk from v₀ to w, there is an a with G.adj v₀ a or G.adj v₁ a, and a ¬∈{v₀,v₁}
-For each walk from v₁ to w, there is an a with G.adj v₀ a or G.adj v₁ a, and a ¬∈{v₀,v₁}
--/
-
 lemma reverse_cons_length {V : Type} {G : simple_graph V} (a v₀ w : V)
   (p' : G.walk a  w)
   (p  : G.walk v₀ w)
@@ -42,81 +13,35 @@ lemma reverse_cons_length {V : Type} {G : simple_graph V} (a v₀ w : V)
                 ... = _                                    : hp,
   nat.succ.inj this
 
-theorem third_vertex_in_walk_induction {V : Type} {G : simple_graph V} (v₀ v₁ w : V)
-(hv₀w: v₀ ≠ w) (hv₁w: v₁ ≠ w) (n:ℕ):
-(∀ p : G.walk v₀ w, p.length = n → ∃ a, (G.adj v₀ a ∨ G.adj v₁ a) ∧ a ≠ v₀ ∧ a ≠ v₁) ∧
-(∀ p : G.walk v₁ w, p.length = n → ∃ a, (G.adj v₀ a ∨ G.adj v₁ a) ∧ a ≠ v₀ ∧ a ≠ v₁)
+theorem vertex_in_walk_induction {V : Type} {G : simple_graph V} {S : set V} {w : V}
+(hw: ¬ w ∈ S) (n:ℕ):
+∀ t, t ∈ S →  (∀ p : G.walk t w, p.length = n → ∃ a, ∃ u, u ∈ S ∧ (G.adj u a) ∧ ¬ a ∈ S)
 :=
 nat.rec_on n (
-  and.intro
-    (λ _ hpl, false.elim (hv₀w (simple_graph.walk.eq_of_length_eq_zero hpl)))
-    (λ _ hpl, false.elim (hv₁w (simple_graph.walk.eq_of_length_eq_zero hpl)))
-) (λ k h_ind, (and.intro (
-  λ p hp,
-  exists.elim (simple_graph.walk.exists_eq_cons_of_ne hv₀w p) (
-    λ a ha, exists.elim ha (
-      λ h hh, exists.elim hh (
-        exists.elim hh (
-          λ p' hp' _ _, 
-          have h_can_ind: p'.length = k, from reverse_cons_length a v₀ w p' p h hp hp',
-          --rw the type of p' when a is v₀ or v₁
-          (classical.em (a = v₀)).elim (λheq, -- case a is v₀. 
-              h_ind.1 ((rewrite ⟨p',h_can_ind⟩ heq)) (rewrite ⟨p',h_can_ind⟩ heq).2
-          ) ( λ hneq0, 
-            (classical.em (a=v₁)).elim (λheq, -- case a is v₁ (basically identical to the v₀ case)
-              h_ind.2 ((rewrite ⟨p',h_can_ind⟩ heq)) (rewrite ⟨p',h_can_ind⟩ heq).2
-            ) (λ hneq1,-- case a is neither v₀ nor v₁
-              exists.intro a (and.intro (or.inl h) (and.intro hneq0 hneq1))
-            )
-          )
-        )
-      )
-    )
-  )
-) (
-    λ p hp,-- exactly same with v₀, v₁ switched:
-    exists.elim (simple_graph.walk.exists_eq_cons_of_ne hv₁w p) (
-      λ a ha, exists.elim ha (
-        λ h hh, exists.elim hh (
-          exists.elim hh (
-            λ p' hp' _ _, 
-          have h_can_ind: p'.length = k, from reverse_cons_length a v₁ w p' p h hp hp',
-              (classical.em (a=v₀)).elim (--rw the type of p' under cases of whether a is v₀ or v₁ or neither
-                λheq, h_ind.1 ((rewrite ⟨p',h_can_ind⟩ heq)) (rewrite ⟨p',h_can_ind⟩ heq).2
-              ) (
-                λ hneq0, 
-                (classical.em (a=v₁)).elim (
-                  λ heq, h_ind.2 ((rewrite ⟨p',h_can_ind⟩ heq)) (rewrite ⟨p',h_can_ind⟩ heq).2
-                ) (λ hneq1,
-                  exists.intro a (and.intro (
-                    or.inr h
-                  ) (and.intro hneq0 hneq1))
-                )
-              )
-            )
-          )
+    (λ v hv _ hpl,
+    have v ≠ w, from by tidy,
+    false.elim (this (simple_graph.walk.eq_of_length_eq_zero hpl)))
+)
+(λ k h_ind v hv p hp,
+    have v ≠ w, from by tidy,
+    exists.elim (simple_graph.walk.exists_eq_cons_of_ne this p) (
+      λ b hb, exists.elim hb (λ h hh, exists.elim hh (
+        λ p' hp',
+        have h_can_ind: p'.length = k, from reverse_cons_length b v w p' p h hp hp',
+        (classical.em (b ∈ S)).elim (
+          λ heq,
+          h_ind b heq p' h_can_ind
+        ) (
+          λ hneq, exists.intro b (exists.intro v (and.intro hv (and.intro h hneq)))
         )
       )
     )
   )
 )
 
-
-theorem third_vertex_in_walk' {V : Type} (G : simple_graph V) (v₀ v₁ w : V)
-   (hv₀w: v₀ ≠ w) (hv₁w: v₁ ≠ w):
-  (∀ p : G.walk v₀ w,  ∃ a, (G.adj v₀ a ∨ G.adj v₁ a) ∧ a ≠ v₀ ∧ a ≠ v₁) :=
-
-    λ p, (third_vertex_in_walk_induction v₀ v₁ w hv₀w hv₁w p.length).1 p rfl
-
-
-theorem third_vertex_in_walk {V : Type} (G : simple_graph V) (v₀ v₁ w : V)
-   (hv₀w: v₀ ≠ w) (hv₁w: v₁ ≠ w):
-  (∀ p : G.walk v₀ w,  ∃ a, (G.adj v₀ a ∨ G.adj v₁ a) ∧ a ≠ v₀ ∧ a ≠ v₁) ∧
-  (∀ p : G.walk v₁ w,  ∃ a, (G.adj v₀ a ∨ G.adj v₁ a) ∧ a ≠ v₀ ∧ a ≠ v₁) :=
-
-  and.intro (
-    λ p, (third_vertex_in_walk_induction v₀ v₁ w hv₀w hv₁w p.length).1 p rfl
-  ) (
-    λ p, (third_vertex_in_walk_induction v₀ v₁ w hv₀w hv₁w p.length).2 p rfl
-  )
-
+theorem vertex_in_walk {V : Type} {G : simple_graph V} (S : set V) (w t : V)
+(hw: ¬ w ∈ S) (ht: t ∈ S)
+(p : G.walk t w):
+∃ a, ∃ u, u ∈ S ∧ (G.adj u a) ∧ ¬ a ∈ S
+:=
+vertex_in_walk_induction   hw p.length t ht p rfl
