cb636652354e7868e1563045973cac0d245749fc
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2023-06-16 08:28:06 -1000

    Code improvement

--- almost-squarefree.lean
+++ almost-squarefree.lean
@@ -41,7 +41,7 @@ lemma eq_cons_ne_nil {a b c d : fin 2}
     repeat {cases hcd, exfalso, exact cons_ne_nil' hcd},
     repeat {cases hdc, exfalso, exact cons_ne_nil' hdc},
     exact calc a = d : (list.head_eq_of_cons_eq hdc).symm
-            ... = b :  list.head_eq_of_cons_eq (list.tail_eq_of_cons_eq hcd)
+             ... = b :  list.head_eq_of_cons_eq (list.tail_eq_of_cons_eq hcd)
   }
 
 theorem generalized (w : list (fin 2)) (a b : fin 2)
@@ -69,7 +69,7 @@ def overlapfree (w : list (fin 2)) : Prop :=
 
 def ctrex := ([0,0,0]: list (fin 2))
 
-lemma cons_append_length (a b : fin 2) (x u z : list (fin 2)) :
+lemma cons_append_length {a b : fin 2} {x u z : list (fin 2)} :
   (x       ++ ((a :: (b :: u))   ++ (a :: (b :: u)))   ++ z).length =
   x.length +  u.length.succ.succ +  u.length.succ.succ +  z.length :=
   by {
@@ -79,24 +79,19 @@ lemma cons_append_length (a b : fin 2) (x u z : list (fin 2)) :
     repeat{rw list.length_cons,},
   }
 
-lemma square_length_ge_four (x z u: list (fin 2)) ( a b : fin 2):
-  (x ++ ((a :: (b :: u)) ++ (a::(b::u))) ++ z).length ≥ 4 :=
-  calc _ = x.length + u.length.succ.succ + u.length.succ.succ + z.length : cons_append_length _ _ _ _ _
+lemma square_length_ge_four {x z u: list (fin 2)} {a b : fin 2}:
+  (x ++ ((a :: (b :: u)) ++ (a::(b::u))) ++ z).length ≥ 4 := calc
+    _ = x.length + u.length.succ.succ + u.length.succ.succ + z.length : cons_append_length
   ... ≥ x.length + u.length.succ.succ + u.length.succ.succ            : le_self_add
   ... = x.length + (u.length.succ.succ + u.length.succ.succ)          : add_assoc _ _ _
   ... ≥            u.length.succ.succ + u.length.succ.succ            : le_add_self
   ... =            2*u.length + 4                                     : by ring
   ... ≥ _                                                             : le_add_self
 
-
-lemma square_length_four (x z : list (fin 2)) ( a b : fin 2): (x ++ ([a,b] ++ [a,b]) ++ z).length ≥ 4 :=
-  square_length_ge_four _ _ _ _ _
-
-
-theorem ctrex_ne (x z : list (fin 2)) ( a b : fin 2): ctrex ≠ (x ++ ([a,b] ++ [a,b]) ++ z) :=
+theorem ctrex_ne {x z : list (fin 2)} {a b : fin 2}: ctrex ≠ (x ++ ([a,b] ++ [a,b]) ++ z) :=
   λh, have ctrex.length ≥ 4, from
   calc ctrex.length = (x ++ ([a,b] ++ [a,b]) ++ z).length: by rw h
-                ... ≥ 4: square_length_four _ _ _ _,
+                ... ≥ 4: square_length_ge_four,
   nat.not_succ_le_self 3 this
 
 example: generalized_almost_square_free ctrex := by {
@@ -104,22 +99,21 @@ example: generalized_almost_square_free ctrex := by {
   cases hcontra_left with hn h,
   cases h with x hx,
   cases hx with z hz,
-  exact ctrex_ne x z a b hz,
+  exact ctrex_ne hz,
   intros y hy,
   cases y,
   exfalso,exact hy.1 rfl,
   left,
   cases y_tl,
   existsi y_hd,refl,
-  exfalso,
+  rename y_hd a,rename y_tl_tl u,rename y_tl_hd b,exfalso,
   cases hy with hy1 hy2,
   cases hy2 with x hx,
   cases hx with z hz,
   have : 3 ≥ 4, from calc
     3 = ctrex.length: rfl
-  ... = (x ++ (y_hd :: y_tl_hd :: y_tl_tl ++ y_hd :: y_tl_hd :: y_tl_tl) ++ z).length : by rw hz
-  ... = (x ++ (y_hd :: y_tl_hd :: y_tl_tl ++ y_hd :: y_tl_hd :: y_tl_tl) ++ z).length : by repeat {rw list.append_assoc}
-  ... ≥ 4                                                                             : square_length_ge_four x z y_tl_tl y_hd y_tl_hd,
+  ... = (x ++ (a :: b :: u ++ a :: b :: u) ++ z).length : by rw hz
+  ... ≥ 4                                               : square_length_ge_four,
   exact nat.not_succ_le_self 3 this,
 }
 
