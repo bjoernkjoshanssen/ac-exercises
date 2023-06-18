1cd8356c606c87e40ebe8e895cb4583f5c8edc2a
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2023-06-18 13:15:28 -1000

    Update max-exercise.lean

--- max-exercise.lean
+++ max-exercise.lean
@@ -1,3 +1,6 @@
+import tactic.basic
+import tactic.ring
+
 lemma max_left {a b b' c c' : ℕ} (h: a ≤ b * c):
     a ≤ (max b b') * max c c' := calc
     a ≤ b * c: h
@@ -14,3 +17,16 @@ theorem max_ineq (a a' b b' c c' : ℕ) (h: a ≤ b * c) (h' : a' ≤ b' * c'):
 max a a' ≤ (max b b') * max c c' :=
 max_le (max_left h) (max_right h')
 
+theorem mul_ineq (a a' b b' c c' : ℕ) (h: a ≤ b * c) (h' : a' ≤ b' * c'):
+  a * a' ≤ (b * b') * (c * c') :=
+
+  calc a * a' ≤ (b*c) * a'          : nat.mul_le_mul_right _ h
+          ... ≤ (b*c) * (b' * c')   : nat.mul_le_mul_left _ h'
+          ... = (b * b') * (c * c') : by ring
+
+theorem add_ineq (a a' b b' c c' : ℕ) (h: a ≤ b + c) (h' : a' ≤ b' + c'):
+  a + a' ≤ (b + b') + (c + c') :=
+
+  calc a + a' ≤ (b+c) + a'          : nat.add_le_add_right h  _
+          ... ≤ (b+c) + (b' + c')   : nat.add_le_add_left  h' _
+          ... = (b + b') + (c + c') : by ring
