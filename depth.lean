c3341799f707ffd262d3b40de730bbd31d1ca8cb
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2023-11-08 13:09:49 -1000

    Lean 4 version

--- depth.lean
+++ depth.lean
@@ -1,5 +1,7 @@
-import ring_theory.int.basic
-import ring_theory.principal_ideal_domain
+import Mathlib.Init.Data.Int.Basic
+import Mathlib.Tactic.Ring
+import Mathlib.Algebra.GCDMonoid.Basic
+import Mathlib.RingTheory.Int.Basic
 
 def sat_eq (a: ℤ×ℤ) (n: ℤ) (x: ℤ×ℤ) : Prop :=
 a.1*x.1+a.2*x.2=n
@@ -11,44 +13,50 @@ theorem depth1 {a b x y k n : ℤ}
   (hsat:sat_eq (a,b) n (x,y)):
   sat_eq (a,b) n (x-k*b,y+k*a) :=
   calc  a*(x-k*b)+b*(y+k*a)
-      = a*x+b*y: by ring
-  ... = n: hsat
+      = a*x+b*y := by ring
+  _ = n := hsat
   
   theorem depth2 {a b n : ℤ}
-  (h : gcd_monoid.gcd a b = 1): ∃ x y : ℤ, sat_eq (a,b) n (x,y) := by {
-  have : ∃ (x y), gcd_monoid.gcd a b = a * x + b * y, from exists_gcd_eq_mul_add_mul _ _,
-  cases this with x hx,
-  cases hx with y hy,
-  have : a*x+b*y = 1, from eq.trans hy.symm h,
-  existsi [x*n, y*n],
-  exact calc a*(x*n)+b*(y*n) = (a*x+b*y)*n: by ring
-  ... = 1*n: by rw this
-  ... = n: by ring
+  (h : GCDMonoid.gcd a b = 1): ∃ x y : ℤ, sat_eq (a,b) n (x,y) := by {
+  have unknown: ∃ x y, GCDMonoid.gcd a b = a * x + b * y :=
+    exists_gcd_eq_mul_add_mul _ _
+  rcases unknown with ⟨x, hx⟩
+  rcases hx with ⟨y, hy⟩
+  have : a*x+b*y = 1 := Eq.trans hy.symm h
+  use x*n
+  use y*n
+  --use [x*n, y*n]
+  exact calc a*(x*n)+b*(y*n) = (a*x+b*y)*n := by ring
+  _ = 1*n := by rw [this]
+  _ = n := by ring
   }
 
 theorem depth3 (a b n : ℤ) 
-(h: gcd_monoid.gcd a b = 1): ∃ x y, ∀ k, sat_eq (a,b) n (x-k*b, y+k*a) :=
+(h: GCDMonoid.gcd a b = 1): ∃ x y, ∀ k, sat_eq (a,b) n (x-k*b, y+k*a) :=
 by {
-  have : ∃ x y : ℤ, sat_eq (a,b) n (x,y), from depth2 h,
-  cases this with x hx,
-  cases hx with y hxy,
-  existsi [x,y],
-  intro k,
+  have : ∃ x y : ℤ, sat_eq (a,b) n (x,y) := depth2 h
+  rcases this with ⟨x, hx⟩
+  rcases hx with ⟨y, hxy⟩
+  use x
+  use y
+  intro k
   exact depth1 hxy
 }
 
 theorem solve3 (a b c n : ℤ) (
-  h : gcd_monoid.gcd (gcd_monoid.gcd a b) c = 1)
+  h : GCDMonoid.gcd (GCDMonoid.gcd a b) c = 1)
   : ∃ x y z : ℤ, sat_eq3 a b c n x y z :=
   by {
-  have h1: ∃ x y : ℤ, sat_eq ((gcd_monoid.gcd a b),c) n (x,y), from depth2 h,
-  have h2: ∃ u v : ℤ, gcd a b = a*u+b*v, from exists_gcd_eq_mul_add_mul _ _,
-  cases h1 with x hx,
-  cases hx with y hxy,
-  cases h2 with u hu,
-  cases hu with v huv,
-  have : (a*u+b*v) * x + c*y = n, by rwa huv at hxy,
-  existsi [u*x, v*x, y],
-  exact calc _ =  (a*u+b*v) * x + c*y: by ring
-           ... = _: this
+  have h1: ∃ x y : ℤ, sat_eq ((GCDMonoid.gcd a b),c) n (x,y) := depth2 h
+  have h2: ∃ u v : ℤ, gcd a b = a*u+b*v := exists_gcd_eq_mul_add_mul _ _
+  rcases h1 with ⟨x, hx⟩
+  rcases hx with ⟨y, hxy⟩
+  rcases h2 with ⟨u, hu⟩
+  rcases hu with ⟨v, huv⟩
+  have : (a*u+b*v) * x + c*y = n := by rwa [huv] at hxy
+  use u*x
+  use v*x
+  use y
+  exact calc _ =  (a*u+b*v) * x + c*y := by ring
+           _ = _ := this
   }
