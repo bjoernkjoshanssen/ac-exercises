88a04ef94d17554aaf3b192c007211ffaba98059
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2023-11-10 09:35:21 -1000

    Lean 4 version of pisano.lean

--- pisano.lean
+++ pisano.lean
@@ -1,8 +1,10 @@
-def fibo (p l : ℕ) : (list ℕ) × (ℕ×ℕ):= -- a list, and its last two elements
+import Mathlib.Data.Nat.Basic
+
+def fibo (p l : ℕ) : (List ℕ) × (ℕ×ℕ):= -- a List, and its last two elements
   ite (p ≤ 1) ([0],0,0)
-  $ nat.rec_on l
+  $ Nat.recOn l
     ([1,0], 1, 0) -- Start of Fibonacci sequence, backwards
-    (λ k, λ fib_ind,
+    (λ _ fib_ind ↦
       ite (fib_ind.2.1 = 1 ∧ (fib_ind.2.2 + 1) % p = 0) fib_ind
       (
         ((fib_ind.2.1 + fib_ind.2.2) % p) :: fib_ind.1,
@@ -11,7 +13,7 @@ def fibo (p l : ℕ) : (list ℕ) × (ℕ×ℕ):= -- a list, and its last two el
       )
     )
 
-def pisano (p : ℕ) : list ℕ := (fibo p (p*p-1)).1.reverse
+def pisano (p : ℕ) : List ℕ := (fibo p (p*p-1)).1.reverse
 
 #eval pisano 1
 #eval pisano 2
