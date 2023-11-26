9e188d4b4ce9e29366e401d5f9a44887e860f069
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2023-11-25 15:13:30 -1000

    Update and rename squarefree.lean to exercise-2-2.lean

--- exercise-2-2.lean
+++ exercise-2-2.lean
@@ -2,6 +2,8 @@ import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Vector.Basic
 import Mathlib.Data.Fintype.Vector
 
+-- Solution to Exercise 2.2.
+
 def squarefree {k:ℕ} (w: Vector (Fin 2) k) : Prop :=
 ∀ l:ℕ, l < w.length → ∀ v : Vector (Fin 2) l,
 v.1 ≠ List.nil → ¬ List.IsInfix (v.1 ++ v.1) w.1
