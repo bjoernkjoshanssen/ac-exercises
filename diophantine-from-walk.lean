aee7974cf500447d54d631c1b59f76ac2bf5dd9b
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2023-12-20 20:16:10 -1000

    Update diophantine-from-walk.lean
    From 2 and 3 to two and three, i.e., arbitrary cycle lengths.

--- diophantine-from-walk.lean
+++ diophantine-from-walk.lean
@@ -9,30 +9,10 @@ set_option tactic.hygienic false
 
 /- Connect the diophantine equation 2x+3y=n with a walk in a digraph that has a cycle of length 2 followed by a cycle of length 3. -/
 
-theorem mod_up {n:ℕ} (h: n % 2 = 0) : n.succ % 2 = 1 := calc
-  _ = ((n%2) + 1%2) %2  := Nat.add_mod _ _ _
-  _ = (0 + 1%2) % 2     := congrFun (congrArg HMod.hMod (congrFun (congrArg HAdd.hAdd h) (1 % 2))) 2
-
-theorem mod_down {n:ℕ} (h: n % 2 = 1) : n.succ % 2 = 0 := calc
-  _ = ((n%2) + 1%2) %2  := Nat.add_mod _ _ _
-  _ = (1 + 1%2) % 2     := congrFun (congrArg HMod.hMod (congrFun (congrArg HAdd.hAdd h) (1 % 2))) 2
-
-theorem mod30 {n:ℕ} (h: n % 3 = 0) : n.succ % 3 = 1 := calc
-  _ = ((n%3) + 1%3) %3  := Nat.add_mod _ _ _
-  _ = (0 + 1%3) % 3     := congrFun (congrArg HMod.hMod (congrFun (congrArg HAdd.hAdd h) (1 % 3))) 3
-
-theorem mod31 {n:ℕ} (h: n % 3 = 1) : n.succ % 3 = 2 := calc
-  _ = ((n%3) + 1%3) %3  := Nat.add_mod _ _ _
-  _ = (1 + 1%3) % 3     := congrFun (congrArg HMod.hMod (congrFun (congrArg HAdd.hAdd h) (1 % 3))) 3
-
-theorem mod32 {n:ℕ} (h: n % 3 = 2) : n.succ % 3 = 0 := calc
-  _ = ((n%3) + 1%3) %3  := Nat.add_mod _ _ _
-  _ = (2 + 1%3) % 3     := congrFun (congrArg HMod.hMod (congrFun (congrArg HAdd.hAdd h) (1 % 3))) 3
-
-
-
-theorem explicit_formula {two:ℕ} {f:ℕ → ℕ}
--- Now we add the possibility of going to state 2... but also rule it out
+theorem explicit_formula {two:ℕ}
+(htwo: 1 < two)
+{f:ℕ → ℕ}
+-- like explicit_formula' but with two instead of 2
 (h00 : f 0 = 0)
 (h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = two)
 (h1: ∀ i t : ℕ, i % two ≠ 0 → f t = i % two → f t.succ = (i.succ) % two)
@@ -40,66 +20,59 @@ theorem explicit_formula {two:ℕ} {f:ℕ → ℕ}
 :
 (∀ s, s ≤ t → f s < two) →
  f t = t % two  := by {
-  sorry -- try to generalize from 2 to "two"
- }
-
-theorem explicit_formula' {f:ℕ → ℕ}
--- Now we add the possibility of going to state 2... but also rule it out
-(h00 : f 0 = 0)
-(h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = 2)
-(h1: ∀ t, f t = 1 → f t.succ = 0)
-(t: ℕ)
-:
-(∀ s, s ≤ t → f s = 0 ∨ f s = 1) →
- f t = t % 2  := by {
   induction t
   rw [h00];
   intro
   rfl
   intro h
-  have hfn : f n = n % 2 := n_ih (λ s hsn ↦ h s (Nat.le_step hsn))
-  have : n % 2 = 0 ∨ n % 2 = 1 := by exact Nat.mod_two_eq_zero_or_one n
-  cases this
-  rw [h_1] at hfn
+  have hfn : f n = n % two := n_ih (λ s hsn ↦ h s (Nat.le_step hsn))
+  rename ∀ (s : ℕ), s ≤ Nat.succ n → f s < two => hs
+  by_cases (n % two = 0)
+  rw [h] at hfn
   let g := h0 n hfn
   cases g
-  rw [h_2]
-  exact (mod_up h_1).symm
-  exfalso
-  let g₀ := h n.succ (le_refl _)
-  cases g₀
-  have h02 : 0 = 2 := Eq.trans h_3.symm h_2
-  have : ¬ 0 = 2 := by exact Nat.ne_of_beq_eq_false rfl
-  exact this h02
-  have h02 : 1 = 2 := Eq.trans h_3.symm h_2
-  have : ¬ 1 = 2 := by exact Nat.ne_of_beq_eq_false rfl
-  exact this h02
-  rw [h_1] at hfn
-  let g := h1 n hfn
-  rw [g]
-  exact (mod_down h_1).symm
-  }
+  rw [h_1]
+  have : (n+1) % two = (n % two + 1 % two) % two := Nat.add_mod _ _ _
+  rw [h,zero_add,Nat.mod_mod] at this
+  have ht: 1 % two = 1 := Nat.mod_eq_of_lt htwo
+  rw [ht] at this
+  exact this.symm
 
-theorem get_even {f:ℕ → ℕ}
--- When do we first go to two:
-(h00 : f 0 = 0)
-(h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = 2)
-(h1: ∀ t, f t = 1 → f t.succ = 0)
-(t: ℕ)
-(h : (∀ s, s ≤ t → f s = 0 ∨ f s = 1))
-(h2: f t.succ = 2):
- t % 2 = 0 := by {
-  have ftt : f t = t % 2 := explicit_formula' h00 h0 h1 _ h
-  have : f t = 0 ∨ f t = 1 := h _ (le_refl _)
-  cases this
-  exact Eq.trans ftt.symm h_1
+  rw [h_1]
   exfalso
-  let g := h1 t h_1
-  have h02 : 0 = 2 := Eq.trans g.symm h2
-  have : ¬ 0 = 2 := by exact Nat.ne_of_beq_eq_false rfl
-  exact this h02
-
+  let gg := hs n.succ (le_refl _)
+  have : two < two := Eq.trans_lt (id h_1.symm) gg
+  exact LT.lt.false this
+  let g := h1 n n h hfn
+  exact g
  }
+
+
+
+theorem get_even' {two:ℕ}
+-- like get_even but with two instead of 2
+(htwo: 1 < two)
+{f:ℕ → ℕ}
+(h00 : f 0 = 0)
+(h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = two)
+(h1: ∀ i t : ℕ, i % two ≠ 0 → f t = i % two → f t.succ = (i.succ) % two)
+(t: ℕ)
+(h : (∀ s, s ≤ t → f s < two))
+(h2: f t.succ = two):
+ t % two = 0 := by {
+  have ftt : f t = t % two := explicit_formula htwo h00 h0 h1 (by {
+    exact t
+  }) h
+  by_contra
+  rename ¬ (t % two = 0) => hcontra
+  let g := h1 t t hcontra ftt
+  have ht1: t.succ % two = two := Eq.trans g.symm h2
+  have ht2: t.succ % two < two := Nat.mod_lt _ (Nat.zero_lt_of_lt htwo)
+  have : two < two := Eq.trans_lt ht1.symm ht2
+  exact LT.lt.false this
+ }
+
+
 
 theorem get_equation (a b n : ℕ) (hab: a ≤ b) (han : (b) % n = a % n):
   ∃ k, b = a + n * k := by {
@@ -113,133 +86,67 @@ theorem get_equation (a b n : ℕ) (hab: a ≤ b) (han : (b) % n = a % n):
     _ = a + n*k := by ring
   }
 
-theorem get_add_mod_three (f:ℕ → ℕ)
-  -- Going through the second cycle:
-  (h2: ∀ t, f t = 2 → f t.succ = 3)
-  (h3: ∀ t, f t = 3 → f t.succ = 4)
-  (h4: ∀ t, f t = 4 → f t.succ = 2)
-  (t: ℕ)
-  (ht2: f t.succ = 2)
-  (s:ℕ)
-  : f (t.succ+s) = 2 + s%3 := by {
-    induction s;exact ht2
-    have : n%3 = 0 ∨ n%3 = 1 ∨ n%3 = 2 := by {
-      by_cases (n%3 < 2);rename n%3 < 2 => hn
-      by_cases (n%3 < 1);left;exact Nat.lt_one_iff.mp h
-      right;left;exact Nat.eq_of_lt_succ_of_not_lt hn h
-      right;right;have : n % 3 < 3 := Nat.mod_lt _ (Nat.succ_pos 2)
-      exact Nat.eq_of_lt_succ_of_not_lt this h
-    }
-    cases this;rw [h] at n_ih;rw [add_zero] at n_ih;let g := h2 (t.succ+n) n_ih
-    rw [mod30 h];exact g;cases h;rw [h_1] at n_ih;let g := h3 (t.succ+n) n_ih
-    rw [mod31 h_1];exact g;rw [h_1] at n_ih;let g := h4 (t.succ+n) n_ih
-    rw [mod32 h_1];exact g
-  }
 
-theorem get_add_mod_three' {three:ℕ} (f:ℕ → ℕ)
+theorem get_add_mod_three'' {two three:ℕ} (f:ℕ → ℕ)
   -- A more general version of get_add_mod_three that doesn't use mod_up
-  (h2s: ∀ i t, f t = 2 + (i%three) → f t.succ = 2 + (i.succ %three))
+  (h2s: ∀ i t, f t = two + (i%three) → f t.succ = two + (i.succ %three))
   (t: ℕ)
-  (ht2: f t.succ = 2)
+  (ht2: f t.succ = two)
   (s:ℕ)
-  : f (t.succ+s) = 2 + s%three := by {
+  : f (t.succ+s) = two + s%three := by {
     induction s;exact ht2
     let g := h2s n (t.succ+n) n_ih
     exact g
   }
-theorem get_mod_three' {three:ℕ} {f:ℕ → ℕ}
+
+theorem get_mod_three'' { two three:ℕ} {f:ℕ → ℕ}
 -- Going through the second cycle:
-  (h2s: ∀ i t, f t = 2 + (i%three) → f t.succ = 2 + (i.succ %three))
+  (h2s: ∀ i t, f t = two + (i%three) → f t.succ = two + (i.succ %three))
 (u v: ℕ)
-(hu: f u.succ = 2)
-(hv: f v.succ = 2)
+(hu: f u.succ = two)
+(hv: f v.succ = two)
 (huv: u ≤ v)
 : u.succ % three = v.succ % three := by {
   have he : ∃ s, v = u + s := by exact Nat.exists_eq_add_of_le huv
   rcases he with ⟨s,hs⟩
-  have : f (u.succ + s) = 2 + s%three := get_add_mod_three' _ h2s _ hu _
+  have : f (u.succ + s) = two + s%three := get_add_mod_three'' _ h2s _ hu _
   rw [hs]
-  rw [Nat.succ_add] at this
-  rw [← hs] at this
-  rw [hv] at this
+  rw [Nat.succ_add,← hs,hv] at this
   have hz : 0 = s % three := by exact Nat.add_left_cancel this
   have hN : Nat.succ (u + s) % three = (Nat.succ u + s) % three := by rw[Nat.succ_add]
   have : (Nat.succ u + s) % three = ((Nat.succ u % three) + s % three) % three:= Nat.add_mod _ _ _
-  rw [hN]
-  rw [this]
-  rw [← hz]
-  rw [Nat.add_zero]
+  rw [hN,this,← hz,Nat.add_zero]
   exact (Nat.mod_mod _ _).symm
 }
-theorem get_multiple_three' {three:ℕ} {f:ℕ → ℕ}
+
+theorem get_multiple_three'' {two three:ℕ} {f:ℕ → ℕ}
   -- Going through the second cycle:
-  (h2s: ∀ i t, f t = 2 + (i%three) → f t.succ = 2 + (i.succ %three))
+  (h2s: ∀ i t, f t = two + (i%three) → f t.succ = two + (i.succ %three))
   (u v: ℕ)
-  (hu: f u.succ = 2)
-  (hv: f v.succ = 2)
+  (hu: f u.succ = two)
+  (hv: f v.succ = two)
   (huv: u ≤ v)
   : ∃ k, v.succ = u.succ + three * k := by {
-    have han : (u.succ) % three = v.succ % three := get_mod_three' h2s _ _ hu hv huv
+    have han : (u.succ) % three = v.succ % three := get_mod_three'' h2s _ _ hu hv huv
     have huv' : u.succ ≤ v.succ := Nat.succ_le_succ huv
     exact get_equation _ _ _ huv' han.symm
   }
 
-
-theorem get_mod_three {f:ℕ → ℕ}
--- Going through the second cycle:
-(h2: ∀ t, f t = 2 → f t.succ = 3)
-(h3: ∀ t, f t = 3 → f t.succ = 4)
-(h4: ∀ t, f t = 4 → f t.succ = 2)
-(u v: ℕ)
-(hu: f u.succ = 2)
-(hv: f v.succ = 2)
-(huv: u ≤ v)
-: u.succ % 3 = v.succ % 3 := by {
-  have he : ∃ s, v = u + s := by exact Nat.exists_eq_add_of_le huv
-  rcases he with ⟨s,hs⟩
-  have : f (u.succ + s) = 2 + s%3 := get_add_mod_three _ h2 h3 h4 _ hu _
-  rw [hs]
-  rw [Nat.succ_add] at this
-  rw [← hs] at this
-  rw [hv] at this
-  have hz : 0 = s % 3 := by exact Nat.add_left_cancel this
-  have hN : Nat.succ (u + s) % 3 = (Nat.succ u + s) % 3 := by rw[Nat.succ_add]
-  have : (Nat.succ u + s) % 3 = ((Nat.succ u % 3) + s % 3) % 3:= Nat.add_mod _ _ _
-  rw [hN]
-  rw [this]
-  rw [← hz]
-  rw [Nat.add_zero]
-  exact (Nat.mod_mod _ _).symm
-}
-
-theorem get_multiple_three {f:ℕ → ℕ}
-  -- Going through the second cycle:
-  (h2: ∀ t, f t = 2 → f t.succ = 3)
-  (h3: ∀ t, f t = 3 → f t.succ = 4)
-  (h4: ∀ t, f t = 4 → f t.succ = 2)
-  (u v: ℕ)
-  (hu: f u.succ = 2)
-  (hv: f v.succ = 2)
-  (huv: u ≤ v)
-  : ∃ k, v.succ = u.succ + 3 * k := by {
-    have han : (u.succ) % 3 = v.succ % 3 := get_mod_three h2 h3 h4 _ _ hu hv huv
-    have huv' : u.succ ≤ v.succ := Nat.succ_le_succ huv
-    exact get_equation _ _ _ huv' han.symm
-  }
-
-theorem get_mul_two {f:ℕ → ℕ}
-  (h00 : f 0 = 0)
-  (h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = 2)
-  (h1: ∀ t, f t = 1 → f t.succ = 0)
-  (t: ℕ)
-  (h : (∀ s, s ≤ t → f s = 0 ∨ f s = 1))
-  (h2: f t.succ = 2):
-  ∃ k, t = 0 + 2 * k := by {
-    have : t % 2 = 0 := (get_even h00 h0 h1 _ h h2)
+theorem get_mul_two'
+ {two:ℕ}
+-- like get_even but with two instead of 2
+(htwo: 1 < two)
+{f:ℕ → ℕ}
+(h00 : f 0 = 0)
+(h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = two)
+(h1: ∀ i t : ℕ, i % two ≠ 0 → f t = i % two → f t.succ = (i.succ) % two)  (t: ℕ)
+  (h : (∀ s, s ≤ t → f s < two))
+  (h2: f t.succ = two):
+  ∃ k, t = 0 + two * k := by {
+    have : t % two = 0 := get_even' htwo h00 h0 h1 _ h h2
     exact get_equation _ _ _ (zero_le _) this
   }
-  -- Let's combine get_multiple_three and get_mul_two to have a diophantine equation:
-  -- t₀ is auxiliary here; later we can argue such t₀ must exist
+
 
 
 theorem least_number_principle
@@ -273,100 +180,147 @@ theorem least_number_principle
   }
 
 
-theorem get_1st_two   (f:ℕ → ℕ)
+theorem get_1st_two {two:ℕ}  (f:ℕ → ℕ)
   (u: ℕ)
-  (hu : f u.succ = 2) : ∃ t₀:ℕ, f t₀.succ = 2 ∧ ∀ v, f v.succ = 2 → t₀ ≤ v
+  (hu : f u.succ = two) : ∃ t₀:ℕ, f t₀.succ = two ∧ ∀ v, f v.succ = two → t₀ ≤ v
   := least_number_principle _ (by {exists u})
 
+theorem zero_of_mod (a n : ℕ) (hn: 1 ≤ n) (ha: a % n  = 0 ) (han: a < n) : a = 0 :=
+  by {
+    have : ∃ k, a = 0 + n * k := get_equation _ _ _ (zero_le _) ha
+    rcases this with ⟨k,hk⟩
+    cases k
+    rw [hk]
+    simp
+    have : n < n := calc
+      _ = 0 + n * 1 := by ring
+      _ ≤ 0 + n * n_1.succ := by {field_simp;exact tsub_add_cancel_iff_le.mp rfl}
+      _ = a := hk.symm
+      _ < n := han
+    exfalso
+    exact LT.lt.false this
+  }
 
-theorem two_of_01   (f:ℕ → ℕ)
+theorem le_of_ne_two  {two:ℕ}
+  -- to strengthen two_of_01' since the latter
+  -- uses f s ≠ two instead of f s < 2 unfortunately
+  (htwo: 1 < two)
+  {f:ℕ → ℕ}
   (h00 : f 0 = 0)
-  (h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = 2)
-  (h1: ∀ t, f t = 1 → f t.succ = 0)
-  (u: ℕ)
-  (hu : f u.succ = 2) : ∃ t₀, (∀ s, s ≤ t₀ → f s = 0 ∨ f s = 1) ∧ f t₀.succ = 2
+  (h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = two)
+  (h1: ∀ i t : ℕ, i % two ≠ 0 → f t = i % two → f t.succ = (i.succ) % two)  (t₀: ℕ)
+  (ht₀ : (∀ s, s ≤ t₀ → f s ≠ two))
+       :  ∀ s, s ≤ t₀ → f s < two := by {
+    induction t₀
+    intro s
+    intro
+    have : s = 0 := by exact Nat.le_zero.mp a
+    rw [this]
+    rw [h00]
+    exact Nat.zero_lt_of_lt htwo
+
+    intro s hs
+    cases hs
+
+    let g := n_ih (by {
+      intro s hsn
+      exact ht₀ _ (by exact Nat.le_step hsn)
+    }) n (le_refl _)
+    by_cases (f n = 0)
+    let gg := h0 n h
+    cases gg
+    rw [h_1]
+    exact htwo
+    let ggg := ht₀ n.succ (le_refl _)
+    exfalso
+    exact ggg h_1
+
+    let g1 := h1 (f n) n (by {
+      contrapose h
+      simp
+      simp at h
+      exact zero_of_mod (f n) (two) (Nat.one_le_iff_ne_zero.mpr (Nat.not_eq_zero_of_lt htwo)) h g
+    }) (by {
+      exact (Nat.mod_eq_of_lt g).symm
+    })
+    rw [g1]
+
+    exact Nat.mod_lt _ (by exact Nat.zero_lt_of_lt htwo)
+    let g := n_ih (by {
+      intro s hsn
+      exact ht₀ _ (by exact Nat.le_step hsn)
+    }) s a
+    exact g
+  }
+
+
+theorem two_of_01' {two:ℕ}
+(htwo: 1 < two)
+-- like two_of_01 but with two instead of 2
+-- it uses f s ≠ two instead of f s < 2 unfortunately
+   {f:ℕ → ℕ}
+  (h00 : f 0 = 0)
+  (h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = two)
+  {u: ℕ}
+  (hu : f u.succ = two) : ∃ t₀, (∀ s, s ≤ t₀ → f s ≠ two) ∧ f t₀.succ = two
   := by {
-    have : ∃ t₀:ℕ, f t₀.succ = 2 ∧ ∀ v, f v.succ = 2 → t₀ ≤ v := get_1st_two _ u hu
+    have : ∃ t₀:ℕ, f t₀.succ = two ∧ ∀ v, f v.succ = two → t₀ ≤ v := get_1st_two _ u hu
     rcases this with ⟨t₀, ht₀⟩
     exists t₀
     constructor
     intro s
     induction s
     intro
-    left
-    exact h00
+    rw [h00]
+    exact Ne.symm (Nat.not_eq_zero_of_lt htwo)
     intro hn
-    have : n ≤ t₀ := by exact Nat.le_of_lt hn
-    let g := n_ih this
-    cases g
-    right
+
+    by_cases (f n = 0)
     let gg := h0 n h
     cases gg
-    exact h_1
+    rw [h_1]
+    exact Nat.ne_of_lt htwo
     exfalso
-    have : t₀ ≤ n := ht₀.2 _ h_1
+    have : t₀ ≤ n := ht₀.2 n h_1
     have : n.succ ≤ n := by exact Nat.le_trans hn this
     exact Nat.le_lt_antisymm this (by exact Nat.lt.base n)
-    left
-    exact h1 n h
+    by_contra
+    let gg := ht₀.2 n a
+    exact Nat.le_lt_antisymm gg hn
     exact ht₀.1
   }
 
-theorem get_diophantine'  (f:ℕ → ℕ)
--- And here we have the equation 2x+3y=7
-  {three : ℕ} -- it works for any cycle length, not just three
+theorem two_of_01'' {two:ℕ}
+  -- like two_of_01 but with two instead of 2
+  (htwo: 1 < two)
+   {f:ℕ → ℕ}
   (h00 : f 0 = 0)
-  (h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = 2)
-  (h1: ∀ t, f t = 1 → f t.succ = 0)
-  (h2s: ∀ i t, f t = 2 + (i%three) → f t.succ = 2 + (i.succ %three))
-  (u: ℕ)
-  (hu : f u.succ = 2)
-  : ∃ k₀ k₁, u.succ = k₀ * 2 + 1 + k₁ * three  := by {
-    have : ∃ t₀, (∀ s, s ≤ t₀ → f s = 0 ∨ f s = 1) ∧ f t₀.succ = 2 := two_of_01 _ h00 h0 h1 u hu
+  (h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = two)
+  (h1: ∀ i t : ℕ, i % two ≠ 0 → f t = i % two → f t.succ = (i.succ) % two)
+  {u: ℕ}
+  (hu : f u.succ = two) : ∃ t₀, (∀ s, s ≤ t₀ → f s < two) ∧ f t₀.succ = two :=
+  by {
+    have : ∃ t₀, (∀ s, s ≤ t₀ → f s ≠ two) ∧ f t₀.succ = two := two_of_01' htwo h00 h0 hu
     rcases this with ⟨t₀,ht₀⟩
-    let h := ht₀.1
-    let ht2 := ht₀.2
-    have ht₀u : t₀ ≤ u := by {
-      have : t₀ ≤ u ∨ u < t₀ := by exact le_or_gt t₀ u
-      cases this
-      exact h_1
-      have : u.succ ≤ t₀ := by exact h_1
-      let g := h u.succ this
-      cases g
-      exfalso
-      have h02 : 0 = 2 := Eq.trans h_2.symm hu
-      have : ¬ 0 = 2 := by exact Nat.ne_of_beq_eq_false rfl
-      exact this h02
-      have h12 : 1 = 2 := Eq.trans h_2.symm hu
-      have : ¬ 1 = 2 := by exact Nat.ne_of_beq_eq_false rfl
-      exfalso
-      exact this h12
-      }
-    have : ∃ k, t₀ = 0 + 2 * k := get_mul_two h00 h0 h1 _ h ht2
-    rcases this with ⟨k₀,hk₀⟩
-    exists k₀
-    have : ∃ k, u.succ = t₀.succ + three * k := get_multiple_three' h2s _ _ ht2 hu ht₀u
-    rcases this with ⟨k₁,hk₁⟩
-    exists k₁
-    rw [hk₁]
-    rw [hk₀]
-    rw [zero_add]
-    linarith
+    exists t₀
+    constructor
+    exact le_of_ne_two htwo h00 h0 h1 _ ht₀.1
+    exact ht₀.2
   }
 
 
-theorem get_diophantine  (f:ℕ → ℕ)
--- And here we have the equation 2x+3y=7
+theorem get_diophantine''  (f:ℕ → ℕ)
+-- like get_diophantine' but with two and three instead of 2 and 3 [MAIN THEOREM]
+  {two three : ℕ}
+  {htwo : 1 < two}
   (h00 : f 0 = 0)
-  (h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = 2)
-  (h1: ∀ t, f t = 1 → f t.succ = 0)
-  (h2: ∀ t, f t = 2 → f t.succ = 3)
-  (h3: ∀ t, f t = 3 → f t.succ = 4)
-  (h4: ∀ t, f t = 4 → f t.succ = 2)
+  (h0: ∀ t, f t = 0 → f t.succ = 1 ∨ f t.succ = two)
+  (h1: ∀ i t : ℕ, i % two ≠ 0 → f t = i % two → f t.succ = (i.succ) % two)
+  (h2s: ∀ i t, f t = two + (i%three) → f t.succ = two + (i.succ %three))
   (u: ℕ)
-  (hu : f u.succ = 2)
-  : ∃ k₀ k₁, u.succ = k₀ * 2 + 1 + k₁ * 3  := by {
-    have : ∃ t₀, (∀ s, s ≤ t₀ → f s = 0 ∨ f s = 1) ∧ f t₀.succ = 2 := two_of_01 _ h00 h0 h1 u hu
+  (hu : f u.succ = two)
+  : ∃ k₀ k₁, u.succ = k₀ * two + 1 + k₁ * three  := by {
+    have : ∃ t₀, (∀ s, s ≤ t₀ → f s < two) ∧ f t₀.succ = two := two_of_01'' htwo h00 h0 h1 hu
     rcases this with ⟨t₀,ht₀⟩
     let h := ht₀.1
     let ht2 := ht₀.2
@@ -374,26 +328,16 @@ theorem get_diophantine  (f:ℕ → ℕ)
       have : t₀ ≤ u ∨ u < t₀ := by exact le_or_gt t₀ u
       cases this
       exact h_1
-      have : u.succ ≤ t₀ := by exact h_1
-      let g := h u.succ this
-      cases g
       exfalso
-      have h02 : 0 = 2 := Eq.trans h_2.symm hu
-      have : ¬ 0 = 2 := by exact Nat.ne_of_beq_eq_false rfl
-      exact this h02
-      have h12 : 1 = 2 := Eq.trans h_2.symm hu
-      have : ¬ 1 = 2 := by exact Nat.ne_of_beq_eq_false rfl
-      exfalso
-      exact this h12
+      have : two < two := by exact Eq.trans_lt (id hu.symm) (h (Nat.succ u) h_1)
+      exact LT.lt.false this
       }
-    have : ∃ k, t₀ = 0 + 2 * k := get_mul_two h00 h0 h1 _ h ht2
+    have : ∃ k, t₀ = 0 + two * k := get_mul_two' htwo h00 h0 h1 _ h ht2
     rcases this with ⟨k₀,hk₀⟩
     exists k₀
-    have : ∃ k, u.succ = t₀.succ + 3 * k := get_multiple_three h2 h3 h4 _ _ ht2 hu ht₀u
+    have : ∃ k, u.succ = t₀.succ + three * k := get_multiple_three'' h2s _ _ ht2 hu ht₀u
     rcases this with ⟨k₁,hk₁⟩
     exists k₁
-    rw [hk₁]
-    rw [hk₀]
-    rw [zero_add]
+    rw [hk₁,hk₀,zero_add]
     linarith
   }
