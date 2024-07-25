510050d19d0ac21e12c5a379351b6f24fd115539
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2024-07-25 09:53:05 -1000

    Add files via upload

--- Acmoi/Exercise1_10.lean
+++ Acmoi/Exercise1_10.lean
@@ -10,12 +10,8 @@ def slow {n:ℕ} {q:ℕ} (h : Mathlib.Vector (Fin q) n.succ): Prop :=
 (h.get 0).1 = 0 ∧
 ∀ j : Fin q, ∃ i : Fin q, i.1 < j.1.succ ∧ (h.get j.1.succ).1 ≤ (h.get i.1).succ
 
-instance {n b:ℕ}  (x : Mathlib.Vector (Fin b) n) (q:ℕ) (h : Mathlib.Vector (Fin q) n.succ):
-Decidable (slow h) :=
-decidable_of_iff (
-(h.get 0).1 = 0 ∧
-∀ j : Fin q, ∃ i : Fin q, i.1 < j.1.succ ∧ (h.get j.1.succ).1 ≤ (h.get i.1).succ
-) (by rw [slow])
+instance {n : ℕ} (q:ℕ) (h : Mathlib.Vector (Fin q) n.succ):
+  Decidable (slow h) := instDecidableAnd
 
 
 def is_witness {n b:ℕ} (x : Mathlib.Vector (Fin b) n) (q:ℕ) (h : Mathlib.Vector (Fin q) n.succ): Prop :=
@@ -37,47 +33,20 @@ def is_witness {n b:ℕ} (x : Mathlib.Vector (Fin b) n) (q:ℕ) (h : Mathlib.Vec
 
 
 instance {n b:ℕ}  (x : Mathlib.Vector (Fin b) n) (q:ℕ) (h : Mathlib.Vector (Fin q) n.succ):
-Decidable (is_witness x q h) :=
-decidable_of_iff (
---   (
---     (h.get 0).1 = 0 ∧
--- ∀ j : Fin q, ∃ i : Fin q, i.1 < j.1.succ ∧ (h.get j.1.succ).1 ≤ (h.get i.1).succ
-
---   ) ∧
-  ∀ g : Mathlib.Vector (Fin q) n.succ,
-    -- slow g →
-
-    g.get 0 = h.get 0 →
-    g.get n = h.get n →
-    ∀ y : Mathlib.Vector (Fin b) n, (
-      ∀ k : Fin n, ∃ l : Fin n,
-        [h.get l.1, h.get l.1.succ] =
-        [g.get k.1, g.get k.1.succ] ∧ x.get l = y.get k
-    ) →
-    g=h ∧ x=y
-) (by rw [is_witness])
+  Decidable (is_witness x q h) := Fintype.decidableForallFintype
 
 
-def A_N_bounded_by
-  {n b :ℕ}
-  (x : Mathlib.Vector (Fin b) n)
-  (q:ℕ) : Prop :=
-∃ h : Mathlib.Vector (Fin q) n.succ, is_witness x q h
+def A_N_bounded_by {n b :ℕ} (x : Mathlib.Vector (Fin b) n) (q:ℕ) : Prop :=
+  ∃ h : Mathlib.Vector (Fin q) n.succ, is_witness x q h
 
-instance   {n b :ℕ}
-  (x : Mathlib.Vector (Fin b) n)
-  (q:ℕ) : Decidable (A_N_bounded_by x q) := decidable_of_iff (
-∃ h : Mathlib.Vector (Fin q) n.succ, is_witness x q h
-  ) (by rw [A_N_bounded_by])
-
+instance {n b q : ℕ} (x : Mathlib.Vector (Fin b) n) :
+  Decidable (A_N_bounded_by x q) := Fintype.decidableExistsFintype
 
 def complexity_equals {n:ℕ} (x:Mathlib.Vector (Fin 2) n) (k:ℕ) : Prop :=
 A_N_bounded_by x k ∧ ¬ A_N_bounded_by x (k-1)
 
 instance {n:ℕ} (x:Mathlib.Vector (Fin 2) n) (k:ℕ) : Decidable (complexity_equals x k) :=
-decidable_of_iff (
-A_N_bounded_by x k ∧ ¬ A_N_bounded_by x (k-1)
-) (by rw [complexity_equals])
+  instDecidableAnd
 
 
 #eval (∀ i ∈ [
